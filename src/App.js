81906aa8fa099680673d3802affbd749ce3a8cd8
Shivam <thisisshivam06@gmail.com>
2022-06-22 14:00:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-Comm Project</h1>
+      <button>Normal Button</button>
+      <Button>Bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
